6c12a3170bb17fc439ec42678976aced132b0b5d
shashank1215 <67216211+shashank1215@users.noreply.github.com>
2020-12-20 02:13:26 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,54 @@
+if ($(window).width() < 600) {
+  $('.col').addClass('btnsm');
+} else {
+  $('.col').addClass('btnlg');
+}
+var correct = [];
+var j = 0;
+var lvl = 1;
+var game = 0;
+var colour = ["green", "red", "yellow", "blue"];
+var audio;
+
+function nxt() {
+  var rand = Math.floor(Math.random() * 4);
+  correct.push(rand);console.log(correct);
+  $("." + colour[rand]).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  audio = new Audio("sounds/" + colour[rand] + ".mp3");
+  audio.play();
+}
+$(document).keydown(function() {
+  $("h1").text("level " + lvl);
+  game = 1;
+  nxt();
+})
+
+$(".btn").click(function() {
+  if (game == 1) {
+    var i = $(this).attr("id");
+    $("." + colour[i]).addClass("pressed");
+    audio = new Audio("sounds/" + colour[i] + ".mp3");
+    audio.play();
+    setTimeout(function() {
+      $("." + colour[i]).removeClass("pressed");
+      if(correct[j]==i){
+        j++;
+        if(j==correct.length){
+        lvl++;
+        j = 0;
+        $("h1").text("level " + lvl);
+        nxt();
+    }}
+    }, 100);
+    if (correct[j] != i) {
+      $("h1").text("Game over!! You reached level " + lvl + ".\n Press any key start again.");
+      audio = new Audio("sounds/wrong.mp3");
+      audio.play();
+      j = 0;
+      lvl = 0;
+      while (correct.length) correct.pop();
+      game = 0;
+
+    }
+  }
+});
